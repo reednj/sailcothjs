ad25131dc7e4ad96a2e91e9d10298740122c0d00
Nathan Reed <reednj@gmail.com>
2017-07-19 12:20:40 +0800

    allow panning around the world canvas

--- demo/src/boxes-world.flow.js
+++ demo/src/boxes-world.flow.js
@@ -3,18 +3,52 @@
 import { Renderable, WorldViewport, StaticViewport } from '../../src/viewport.flow'
 import * as Box from './box.flow'
 
+// just ignore jquery errors
+declare function $(any):any;
+
 export class App {
     viewport:any;
     constructor() {
         let element = document.getElementById('canvas')
+        let move:{x:number,y:number} = {x:0,y:0}
 
         // $FlowFixMe
-        this.viewport = new WorldViewport(element);
+        this.viewport = new WorldViewport(element, {
+            onRedraw: () => { 
+                let p = this.viewport.getCenter();
+                this.viewport.setCenter(p.x + move.x, p.y + move.y);
+             }
+        });
+
         this.viewport.setCenter(0, 0);
         this.viewport.start();
 
-        this.viewport.add(new Box.RotatingBox({ x: 0, y: 0, size: 50 }))
+        this.viewport.add(new Box.RotatingBox({ x: 0, y: 0, size: 64 }));
+        this.viewport.add(new Box.RotatingBox({ x: 0, y: 120, size: 48 }));
+        this.viewport.add(new Box.RotatingBox({ x: 0, y: -120, size: 48 }));
+        this.viewport.add(new Box.RotatingBox({ x: 150, y: 0, size: 48 }));
+        this.viewport.add(new Box.RotatingBox({ x: -150, y: 0, size: 48 }));
         this.viewport.add(new GridLines());
+
+        $(window).keydown((e) => {
+            if(e.key == 'w') {
+                move.y = 1;
+            } else if(e.key == 's') {
+                 move.y = -1;
+            } else if(e.key == 'a') {
+                move.x = -1;
+            } else if(e.key == 'd') {
+                move.x = 1;
+            }
+        });
+
+        $(window).keyup((e) => {
+            if(e.key == 'w' || e.key == 's') {
+                move.y = 0;
+            } else if(e.key == 'a' || e.key == 'd') {
+                move.x = 0;
+            }
+        });
     }
 }
 
